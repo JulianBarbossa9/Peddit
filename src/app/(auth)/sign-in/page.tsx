f00be3f256c47d9148c3e96e1206de96e727c492
Julian Barbosa <julianbarbossa@hotmail.com>
2023-09-16 13:26:05 -0500

    update style for botton home

--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -1,6 +1,7 @@
 import SignIn from "@/components/SignIn";
 import { buttonVariants } from "@/components/ui/Button";
 import { cn } from "@/lib/utils";
+import { ChevronLeft } from "lucide-react";
 import Link from "next/link";
 import { FC } from "react";
 
@@ -15,7 +16,11 @@ const page: FC<pageProps> = () => {
           className={cn(buttonVariants({ variant: "ghost" }),
           "self-start -mt-20"
           )}
-        >Home</Link>
+        >
+          <ChevronLeft  className="mr-2 w-4 h-4"/>
+          Home
+        
+        </Link>
         <SignIn />
       </div>
     </div>
