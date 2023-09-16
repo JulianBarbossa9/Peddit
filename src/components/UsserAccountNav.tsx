a6a2952dd1d5503b83896c32c15e301acdc8b78f
Julian Barbosa <julianbarbossa@hotmail.com>
2023-09-15 21:36:20 -0500

    update the usserAccountNav to sign out

--- src/components/UsserAccountNav.tsx
+++ src/components/UsserAccountNav.tsx
@@ -1,7 +1,11 @@
+"use client"
+
 import { User } from "next-auth";
 import { FC } from "react";
-import { DropdownMenu, DropdownMenuTrigger } from "./ui/DropDownMenu";
+import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuSeparator, DropdownMenuTrigger } from "./ui/DropDownMenu";
 import UserAvatar from "./UserAvatar";
+import Link from "next/link";
+import { signOut } from "next-auth/react";
 
 interface UsserAccountNavProps {
   user: Pick<User, 'name' | 'image' | 'email' >
@@ -20,6 +24,41 @@ const UsserAccountNav: FC<UsserAccountNavProps> = ({ user }) => {
           }} 
         />
       </DropdownMenuTrigger>
+      <DropdownMenuContent className="bg-white" align="end">
+          <div className="flex items-center justify-start gap-2 p-2">
+            <div className="flex flex-col space-y-1 leading-none">
+              { user.image && <p className="font-medium">{user.name}</p>}
+              { user.email && <p className="w-[200px] truncate text-sm">{user.email}</p>}
+            </div>
+          </div>
+          <DropdownMenuSeparator />
+
+          <DropdownMenuItem asChild>
+            <Link href={'/'}>
+              Feed
+            </Link>
+          </DropdownMenuItem>
+          <DropdownMenuItem asChild>
+            <Link href={'/r/create'}>
+              Create Community
+            </Link>
+          </DropdownMenuItem>
+          <DropdownMenuItem asChild>
+            <Link href={'/settings'}>
+              Settings
+            </Link>
+          </DropdownMenuItem>
+          <DropdownMenuSeparator />
+          <DropdownMenuItem onSelect={(event) => {
+            event.preventDefault()
+            signOut({
+              callbackUrl: `${window.location.origin}/sign-in`,
+            })
+          }} className="cursor-pointer">
+            Sign Out
+          </DropdownMenuItem>
+
+      </DropdownMenuContent>
     </DropdownMenu>
   );
 }
