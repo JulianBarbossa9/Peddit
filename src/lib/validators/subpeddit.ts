16be7263c48cab847f8b73f318153b7aa1d2d087
Julian Barbosa <julianbarbossa@hotmail.com>
2023-09-18 21:09:36 -0500

    add validation shema for request

---
+++ src/lib/validators/subpeddit.ts
@@ -0,0 +1,12 @@
+import { z } from 'zod'
+
+export const SubpedditValidator = z.object({
+  name: z.string().min(3).max(21), //ssr validation
+})
+
+export const SubpedditSubscriptionValidator = z.object({
+  subpedditId: z.string()
+})
+
+export type CreateSubpedditPayload = z.infer<typeof SubpedditValidator>
+export type SubcribeToSubpedditPayload = z.infer<typeof SubpedditValidator>
