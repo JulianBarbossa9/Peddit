21aec307e882414a2a21b04d045b0373d969d881
Julian Barbosa <julianbarbossa@hotmail.com>
2023-09-19 13:19:52 -0500

    add subpeddit page v1

---
+++ src/app/p/[slug]/page.tsx
@@ -0,0 +1,15 @@
+import { FC } from "react";
+
+interface subpedditPageProps {
+  
+}
+ 
+const subpedditPage: FC<subpedditPageProps> = () => {
+  return (  
+    <div>
+      subpedditPage
+    </div>
+  );
+}
+ 
+export default subpedditPage;
