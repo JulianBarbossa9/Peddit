8b79ec8bfe27650ab3f21a2970f66d0ddaf0abe5
Julian Barbosa <julianbarbossa@hotmail.com>
2023-09-27 18:54:57 -0500

    add editor component

--- src/components/Editor.tsx
+++ src/components/Editor.tsx
@@ -1,15 +1,76 @@
 "use client"
 
-import { FC } from "react";
+import { FC, useCallback, useRef } from "react";
 import TextareaAutosize from 'react-textarea-autosize'
 import { useForm } from 'react-hook-form'
-import { PostCreationRequest } from "@/lib/validators/post";
+import { PostCreationRequest, PostValidator } from "@/lib/validators/post";
+import { zodResolver } from '@hookform/resolvers/zod'
+import type EditorJS from "@editorjs/editorjs";
 
-interface EditorProps {}
+interface EditorProps {
+  subpedditId: string
+}
 
-const Editor: FC<EditorProps> = () => {
-  
-  const { } = useForm<PostCreationRequest>
+const Editor: FC<EditorProps> = ({ subpedditId }) => {
+  //PostValidator == schema
+  const { 
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<PostCreationRequest>({
+    resolver: zodResolver(PostValidator),
+    defaultValues: {
+      subpedditId,
+      title: '',
+      content: null,
+    }
+  })
+
+  const ref = useRef<EditorJS>()
+
+  const initializeEditor = useCallback( async () => {
+    const EditorJS = ( await import('@editorjs/editorjs')).default
+    const Header = ( await import('@editorjs/header')).default
+    const Embed = ( await import('@editorjs/embed')).default
+    const Table = ( await import ('@editorjs/table')).default
+    const List = ( await import ('@editorjs/list')).default
+    const Code = ( await import ('@editorjs/code')).default
+    const LinkTool = ( await import ('@editorjs/link')).default
+    const InlineCode = ( await import ('@editorjs/inline-code')).default
+    const ImageTool = ( await import ('@editorjs/image')).default
+
+    //Editor is not currently initialized
+    if (!ref.current){
+      const editor = new EditorJS({
+        holder: 'editor',
+        onReady() {
+          ref.current = editor
+        },
+        placeholder: 'Type here to write your post...',
+        inlineToolbar: true,
+        data: { blocks: [] },
+        tools: {
+          header: Header,
+          LinkTool: {
+            class: LinkTool,
+            config: {
+              endpoint: '/api/link'
+            },
+          },
+          image: {
+            class: ImageTool,
+            config: {
+              uploader: {
+                async uploadByFile(file: File){
+                  
+                }
+              }
+            }
+          }
+        },
+      })
+    }
+  }, [])
   
   return (
     <div className="w-full p-4 bg-zing-50 rounded-lg border border-zinc-200">
