9054f6ba72e2f370d4f74fa326bd03f63c275c8d
Julian Barbosa <julianbarbossa@hotmail.com>
2023-09-28 23:22:04 -0500

    create component post v1

---
+++ src/components/Post.tsx
@@ -0,0 +1,15 @@
+import { FC } from "react";
+
+interface PostProps {
+  
+}
+ 
+const Post: FC<PostProps> = () => {
+  return (  
+    <div className="mt-4">
+      hello posts
+    </div>
+  );
+}
+ 
+export default Post;
