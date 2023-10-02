66916db2a7115a0b525551c30e6393f9894afb68
Julian Barbosa <julianbarbossa@hotmail.com>
2023-10-02 16:33:05 -0500

    create votation post

--- src/app/api/supeddit/post/vote/route.ts
+++ src/app/api/supeddit/post/vote/route.ts
@@ -1,90 +1,127 @@
-import { getAuthSession } from "@/lib/auth"
-import { db } from "@/lib/db"
-import { PostVoteValidator } from "@/lib/validators/vote"
-import { CachedPost } from "@/types/redis"
+import { getAuthSession } from "@/lib/auth";
+import { db } from "@/lib/db";
+import { redis } from "@/lib/redis";
+import { PostVoteValidator } from "@/lib/validators/vote";
+import { CachedPost } from "@/types/redis";
+import { z } from "zod";
 
+const CACHE_AFTER_UPVOTES = 1;
 
-const CACHE_AFTER_UPVOTES = 1
-
-export async function PATCH(req:Request) {
+export async function PATCH(req: Request) {
   try {
-    const body = req.json()
+    const body = await req.json();
 
-    const {postId, voteType } = PostVoteValidator.parse(body)
+    const { postId, voteType } = PostVoteValidator.parse(body);
 
-    const session = await getAuthSession()
+    const session = await getAuthSession();
 
-    if(!session?.user) {
-      return new Response('Unauthorized', { status: 404 })
+    if (!session?.user) {
+      return new Response("Unauthorized", { status: 404 });
     }
 
     const existingVote = await db.vote.findFirst({
       where: {
         userId: session.user.id,
-        postId
-      }
-    })
+        postId,
+      },
+    });
 
     const post = await db.post.findUnique({
       where: {
-        id: postId
+        id: postId,
       },
       include: {
         author: true,
         votes: true,
-      }
-    })
+      },
+    });
 
     if (!post) {
-      return new Response('Post not found', { status: 404 })
+      return new Response("Post not found", { status: 404 });
     }
 
-    if(existingVote) {
+    if (existingVote) {
       if (existingVote.type === voteType) {
         await db.vote.delete({
           where: {
             userId_postId: {
               postId,
-              userId: session.user.id
-            }
-          }
-        })
-        return new Response('Vote update sussesfully', { status: 201 })
+              userId: session.user.id,
+            },
+          },
+        });
+        return new Response("Vote update sussesfully", { status: 201 });
       }
 
       await db.vote.update({
         where: {
           userId_postId: {
             postId,
-            userId: session.user.id
+            userId: session.user.id,
           },
         },
         data: {
           type: voteType,
-        }
-      })
+        },
+      });
 
       //recount the votes
       const voteAmt = post.votes.reduce((acc, vote) => {
-        if(vote.type === 'UP') return acc + 1
-        if(vote.type === 'DOWN') return acc - 1
-        return acc
-      }, 0)
+        if (vote.type === "UP") return acc + 1;
+        if (vote.type === "DOWN") return acc - 1;
+        return acc;
+      }, 0);
 
       if (voteAmt >= CACHE_AFTER_UPVOTES) {
         const cachePayload: CachedPost = {
-          authorUsername: post.author.username ?? '',
+          authorUsername: post.author.username ?? "",
           content: JSON.stringify(post.content),
           id: post.id,
           title: post.title,
           currentVote: voteType,
-          createdAt: post.createdAt
-        }
+          createdAt: post.createdAt,
+        };
+
+        await redis.hset(`post:${postId}`, cachePayload);
       }
+      return new Response("Data is cached", { status: 201 });
     }
 
+    //If don't exist a vote
+    await db.vote.create({
+      data: {
+        type: voteType,
+        userId: session.user.id,
+        postId,
+      },
+    });
 
+    const voteAmt = post.votes.reduce((acc, vote) => {
+      if (vote.type === "UP") return acc + 1;
+      if (vote.type === "DOWN") return acc - 1;
+      return acc;
+    }, 0);
+
+    if (voteAmt >= CACHE_AFTER_UPVOTES) {
+      const cachePayload: CachedPost = {
+        authorUsername: post.author.username ?? "",
+        content: JSON.stringify(post.content),
+        id: post.id,
+        title: post.title,
+        currentVote: voteType,
+        createdAt: post.createdAt,
+      };
+      await redis.hset(`post:${postId}`, cachePayload);
+    }
+
+    return new Response("OK", { status: 200 });
   } catch (error) {
-    
+    if (error instanceof z.ZodError) {
+      return new Response("Invalid POST request data passed", { status: 422 });
+    }
+
+    return new Response("Could not register your vote, please try again.", {
+      status: 500,
+    });
   }
 }
