b4114a398979c974b6d18d91c3f47041f777f990
Julian Barbosa <julianbarbossa@hotmail.com>
2023-10-03 12:50:33 -0500

    add compent customerFeed and generalFeed if is subscribed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,5 @@
+import CustomFeed from "@/components/CustomFeed";
+import GeneralFeed from "@/components/GeneralFeed";
 import { buttonVariants } from "@/components/ui/Button";
 import { getAuthSession } from "@/lib/auth";
 import { HomeIcon } from "lucide-react";
@@ -10,7 +12,8 @@ export default async function Home() {
   return <>
     <h1 className="font-bold text-3xl md:text-4xl">Your Feed</h1>
     <div className="grid grid-cols-1 md:grid-cols-3 gap-y-4 md:gap-x-4 py-6">
-      {/* { session ? <CustoomFeed /> : <GeneralFeed />} */}
+      {/*@ts-expect-error server component */}
+      { session ? <CustomFeed /> : <GeneralFeed />}
 
       {/* subpeddit info */}
       <div className="overflow-hidden h-fit border border-gray-200 order-first md:order-last">
