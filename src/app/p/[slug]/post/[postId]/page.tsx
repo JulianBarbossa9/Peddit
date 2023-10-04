64a7a7233cff5b9844d419ea7a28774d408312d7
Julian Barbosa <julianbarbossa@hotmail.com>
2023-10-04 16:52:48 -0500

    add page for detail post and comments

--- src/app/p/[slug]/post/[postId]/page.tsx
+++ src/app/p/[slug]/post/[postId]/page.tsx
@@ -1,7 +1,10 @@
+import CommentsSection from "@/components/CommentsSection";
+import EditorOutput from "@/components/EditorOutput";
 import PostVoteServer from "@/components/post-vote/PostVoteServer";
 import { buttonVariants } from "@/components/ui/Button";
 import { db } from "@/lib/db";
 import { redis } from "@/lib/redis";
+import { formatTimeToNow } from "@/lib/utils";
 import { CachedPost } from "@/types/redis";
 import { Post, User, Vote } from "@prisma/client";
 import { ArrowBigDown, ArrowBigUp, Loader2 } from "lucide-react";
@@ -40,37 +43,62 @@ const DetailPost = async ({ params }: DetailPostProps) => {
     <div>
       <div className="h-full flex flex-col sm:flex-row items-center sm:items-start justify-between">
         <Suspense fallback={<PostVoteShell />}>
-        {/*@ts-expect-error server component */}
-        <PostVoteServer postId={post?.id ?? cachedPost.id} getData={async () => {
-          return await db.post.findUnique({
-            where: {
-              id: params.postId
-            },
-            include: {
-              votes: true,
-            }
-          })
-        }} />
+          {/*@ts-expect-error server component */}
+          <PostVoteServer
+            postId={post?.id ?? cachedPost.id}
+            getData={async () => {
+              return await db.post.findUnique({
+                where: {
+                  id: params.postId,
+                },
+                include: {
+                  votes: true,
+                },
+              });
+            }}
+          />
         </Suspense>
+
+        <div className="sm:w-0 w-full flex-1 bg-white p-4 rounded-sm">
+          <p className="max-h-40 mt-1 truncate text-xs text-gray-500">
+            Posted by u/{post?.author.username ?? cachedPost.authorUsername}{" "}
+            {formatTimeToNow(new Date(post?.createdAt ?? cachedPost.createdAt))}
+          </p>
+          <h1 className="text-xl font-semibold py-2 leading-6 text-gray-900">
+            {post?.title ?? cachedPost.title}
+          </h1>
+
+          <EditorOutput content={post?.content ?? cachedPost.content} />
+          <Suspense
+            fallback={
+              <Loader2 className="w-5 h-5 animate-spin text-zinc-500" />
+            }
+          >
+            {/* @ts-expect-error server component */}
+            <CommentsSection postId={post?.id ?? cachedPost.id} />
+          </Suspense>
+        </div>
       </div>
     </div>
   );
 };
 
 function PostVoteShell() {
-  return <div className="flex items-center flex-col pr-6 w-20">
-    <div className={buttonVariants({ variant: 'ghost'})}>
-      <ArrowBigUp className="w-5 h-5 text-zinc-700" />
+  return (
+    <div className="flex items-center flex-col pr-6 w-20">
+      <div className={buttonVariants({ variant: "ghost" })}>
+        <ArrowBigUp className="w-5 h-5 text-zinc-700" />
+      </div>
+      {/* score */}
+      <div className="text-center py-2 font-medium text-sm text-zinc-900">
+        <Loader2 className="h-3 w-3 animate-spin" />
+      </div>
+      {/* downvote */}
+      <div className={buttonVariants({ variant: "ghost" })}>
+        <ArrowBigDown className="w-5 h-5 text-zinc-700" />
+      </div>
     </div>
-    {/* score */}
-    <div className="text-center py-2 font-medium text-sm text-zinc-900">
-      <Loader2 className="h-3 w-3 animate-spin" />
-    </div>
-    {/* downvote */}
-    <div className={buttonVariants({ variant: 'ghost'})}>
-      <ArrowBigDown className="w-5 h-5 text-zinc-700" />
-    </div>
-  </div>
+  );
 }
 
 export default DetailPost;
